919ad134a995ad0c8c0ecc063b98e8ccec0607dc
EVWatson <emily.watson@myob.com>
2019-06-12 09:34:25 +1000

    number generator tests

--- tests/NumberGeneratorTest.java
+++ tests/NumberGeneratorTest.java
@@ -2,6 +2,7 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 
+
 import static org.junit.Assert.*;
 
 public class NumberGeneratorTest {
@@ -17,13 +18,16 @@ public class NumberGeneratorTest {
         assertEquals(expectedResult, actualResult);
     }
 
+
+//    not sure how to improve this test to say it returns four digits between 1 and 9 only
     @Test
     public void generateFourDigitRandomNumberReturnsNewArrayListWithFourDigitsBetweenAnd1And9(){
         NumberGenerator numberGenerator = new NumberGenerator();
 
         ArrayList<Integer> randomDigits = numberGenerator.generateFourDigitRandomNumber();
 
-        assertTrue(randomDigits.contains(1) || randomDigits.contains(2) || randomDigits.contains(3) || randomDigits.contains(4) ||
+
+        assertTrue(randomDigits.contains(1)|| randomDigits.contains(2) || randomDigits.contains(3) || randomDigits.contains(4) ||
                 randomDigits.contains(5) || randomDigits.contains(6) || randomDigits.contains(7) || randomDigits.contains(8) || randomDigits.contains(9)
         );
     }
@@ -40,7 +44,6 @@ public class NumberGeneratorTest {
                 randomDigits.get(0).equals(randomDigits.get(1)) && randomDigits.get(0).equals(randomDigits.get(2)) && randomDigits.get(0).equals(randomDigits.get(3)) &&
                 randomDigits.get(1).equals(randomDigits.get(2)) && randomDigits.get(1).equals(randomDigits.get(3))&& randomDigits.get(2).equals(randomDigits.get(3))
         );
-
     }
 
 }
